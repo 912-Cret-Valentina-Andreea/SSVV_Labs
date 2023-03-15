0c569b3dc56fcf30e7f7c1bebc1f15ca531411fa
Alexandru Manghiuc <40300847+Alex62493@users.noreply.github.com>
2023-03-15 15:21:17 +0200

    Add files via upload

---
+++ src/test/java/service/StudentTest.java
@@ -0,0 +1,11 @@
+package service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @org.junit.jupiter.api.Test
+    void saveStudent() {
+        assertEquals(16, 16);
+    }
+}
